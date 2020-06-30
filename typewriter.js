48c2f5b8019dd677c5b85af1d16857b3a1354cca
Angel-Ho221 <angel.ho@ryerson.ca>
2020-06-30 20:40:21 +0000

    updated typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,5 @@
-const sentence = "hello. it\'s me.";
+const sentence = "hello. it\'s me.\n";
+
 const typewriter = (sentence) => {
   let startTime = 1000;
   for (const char in sentence) {
@@ -6,8 +7,7 @@ const typewriter = (sentence) => {
       // console.log(sentence[char])// prints each letter one at a time
     }, (startTime + 100 * char));
   }
-  setTimeout(() => {process.stdout.write('\n'); // adds a new line after the loop is completed
-  }, (startTime + 100 * (sentence.length - 1))); // this will happen after the last char is printed
+  
 }
 
 typewriter(sentence);
